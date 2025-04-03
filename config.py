246472bdfdcf32ba92a4d59c7c18d12322f29875
Shadow4546 <89007456+Shadow4546@users.noreply.github.com>
2025-04-03 13:08:01 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,9 +1,9 @@
 import os
 
 class Config(object):
-    BOT_TOKEN = os.environ.get("BOT_TOKEN", '7606779341:AAGFfez95csJUfKp4g8FCsv_ok8lIwWZt5c')
-    API_ID = int(os.environ.get("API_ID", '24025791'))
-    API_HASH = os.environ.get("API_HASH", 'b4980ef76018e77ce8d96cde35451fa9')
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", '')
+    API_ID = int(os.environ.get("API_ID", ''))
+    API_HASH = os.environ.get("API_HASH", '')
     AUTH_USER = os.environ.get('AUTH_USERS','7170328188').split(',')
     AUTH_USERS = [int(user_id) for user_id in AUTH_USER]
     HOST = "https://api.masterapi.tech"
